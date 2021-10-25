7db5e7fc204d9bd6a27f588e4713af1f438f28bb
Necryl <necrylthewayfarer@gmail.com>
2021-10-25 14:00:32 +0530

    setting up some stuff

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 
 
+class Touch(Widget):
+    def on_touch_down(self, touch):
+        pass
+
+    def on_touch_move(self, touch):
+        pass
+
+    def on_touch_up(self, touch):
+        pass
+
+
 class MyApp(App):
     def build(self):
         return
