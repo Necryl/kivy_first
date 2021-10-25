9faff483ed4c9fc19a03ee142129b45c2581a1c9
Necryl <necrylthewayfarer@gmail.com>
2021-10-25 16:01:16 +0530

    ok I should just get used to rests for these tuts

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
-
-
-class Touch(Widget):
-    def on_touch_down(self, touch):
-        pass
-
-    def on_touch_move(self, touch):
-        pass
-
-    def on_touch_up(self, touch):
-        pass
 
 
 class MyApp(App):
