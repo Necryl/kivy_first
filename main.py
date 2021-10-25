328bbaba6c3ec3269b2cab9b209538569fd4ce8b
Necryl <necrylthewayfarer@gmail.com>
2021-10-25 12:03:23 +0530

    updated main.py

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from kivy.app import App
 from kivy.uix.button import Button
 
 
+class FunkyButton(Button):
+    def __init__(self, **kwargs):
+        super(FunkyButton, self).__init__(**kwargs)
+        self.text='hello world'
+        self.pos=(100,100)
+        self.size_hint=(.5,.5)
+
+
 class LanguageLearnerApp(App):
     def build(self):
-        return Button(text='Hello World')
+        return FunkyButton(
+            # text='hello world',
+            # pos=(100,100),
+            # size_hint=(.5,.5)
+        )
 
 
 if __name__ == "__main__":
